fa9ab387833cde9d69ae79520a6570ce8f17f056
LEONARDO JESUS ALAJO CHIMBO <leonardo.alajo3585@utc.edu.ec>
2024-02-21 19:43:37 -0500

    model carrera

--- Aplicaciones/Educacion/models.py
+++ Aplicaciones/Educacion/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Carrera(models.Model):
+    id_car=models.AutoField(primary_key=True)
+    nombre_car=models.CharField(max_length=50)
+    fecha_creacion_car=models.DateField()
+    telefono_car=models.CharField(max_length=10)
