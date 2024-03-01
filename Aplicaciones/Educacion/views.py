11e5e150f23b38fd1a7cbedd9c604529938141a3
xavii07 <xaviergaspa123@gmail.com>
2024-03-01 11:22:20 -0500

    clase 11 tabla carreras

--- Aplicaciones/Educacion/views.py
+++ Aplicaciones/Educacion/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Carrera
 
 # Create your views here.
 def inicio(request):
     return render(request, 'index.html')
+
+def carreras(request):
+
+    carrerasDbb = Carrera.objects.all()
+
+    return render(request, 'carreras.html', {
+        "carreras": carrerasDbb
+    })
