01be923f64faed5f873dfc88d8638b8ac84b3462
xavii07 <xaviergaspa123@gmail.com>
2024-03-02 23:08:04 -0500

    feat models

--- Aplicaciones/Educacion/models.py
+++ Aplicaciones/Educacion/models.py
@@ -7,6 +7,7 @@ class Carrera(models.Model):
     fecha_creacion_car=models.DateField()
     telefono_car=models.CharField(max_length=10)
 
+
     def __str__(self):
         return self.nombre_car
 
