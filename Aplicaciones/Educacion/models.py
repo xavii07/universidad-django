a058ab63bae13f1d50b07886230eebceea0bde0a
xavii07 <xaviergaspa123@gmail.com>
2024-08-19 11:44:12 -0500

    feat add model curso

--- Aplicaciones/Educacion/models.py
+++ Aplicaciones/Educacion/models.py
@@ -12,4 +12,11 @@ class Carrera(models.Model):
     def __str__(self):
         return self.nombre_car
 
+class Curso(models.Model):
+    id_cur=models.AutoField(primary_key=True)
+    nivel_cur=models.CharField(max_length=50)
+    aula_cur=models.IntegerField()
+    fk_id_car=models.ForeignKey(Carrera, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.nivel_cur
